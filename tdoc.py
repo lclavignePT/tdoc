404c7b8ce32eb51322aa287dfdba851baa42e23f
Leonardo Lavigne <lclavignept@gmail.com>
2024-11-12 20:00:54 +0000

    inclue uma diretiva de arquivos de interesse que nao tem extencao(ex:Dockerfile, .gitignore)

--- tdoc.py
+++ tdoc.py
@@ -4,47 +4,51 @@ import subprocess
 import os
 import sys
 import yaml
-from datetime import datetime
 import time
 
 def carregar_configuracoes(config_path="config.yml"):
     """
     Carrega as configurações de um arquivo YAML especificado.
-    Retorna listas de pastas excluídas e extensões desejadas, ou listas vazias se o arquivo não existir.
+    Retorna listas de pastas excluídas, extensões desejadas e arquivos específicos.
     """
     if not os.path.exists(config_path):
         print(f"Aviso: Arquivo '{config_path}' não encontrado. Executando apenas o comando tree.")
-        return [], []
+        return [], [], []
     
     with open(config_path, 'r') as file:
         config = yaml.safe_load(file)
     
     pastas_excluidas = config.get('excluded_directories', [])
     extensoes_desejadas = config.get('desired_extensions', [])
+    arquivos_especificos = config.get('specific_files', [])
     
-    if not extensoes_desejadas:
-        print("Aviso: Nenhuma extensão configurada no arquivo config.yml. Executando apenas o comando tree.")
+    if not extensoes_desejadas and not arquivos_especificos:
+        print("Aviso: Nenhuma extensão ou arquivo específico configurado no arquivo config.yml. Executando apenas o comando tree.")
     
-    return pastas_excluidas, extensoes_desejadas
+    return pastas_excluidas, extensoes_desejadas, arquivos_especificos
 
 def executar_comando_tree(caminho_diretorio, pastas_excluidas):
+    # Adiciona a opção -a para incluir arquivos e pastas que começam com "."
     pastas_excluidas_str = "|".join(pastas_excluidas) if pastas_excluidas else ""
-    comando = f"tree -n --dirsfirst {caminho_diretorio} -I '{pastas_excluidas_str}'" if pastas_excluidas_str else f"tree -n --dirsfirst {caminho_diretorio}"
+    comando = f"tree -n --dirsfirst {caminho_diretorio} -I '{pastas_excluidas_str}'" if pastas_excluidas_str else f"tree -a -n --dirsfirst {caminho_diretorio}"
     resultado = subprocess.run(comando, shell=True, text=True, capture_output=True)
     return resultado.stdout
 
-def encontrar_arquivos(caminho_diretorio, extensoes, pastas_excluidas):
+def encontrar_arquivos(caminho_diretorio, extensoes, arquivos_especificos, pastas_excluidas):
     """
     Percorre o diretório especificado e retorna uma lista de arquivos que correspondem
-    às extensões desejadas, excluindo pastas específicas.
+    às extensões desejadas ou aos arquivos específicos, excluindo pastas especificadas.
     """
     arquivos_encontrados = []
     pastas_excluidas_set = set(pastas_excluidas)
     
     for root, dirs, files in os.walk(caminho_diretorio):
+        # Remove pastas que devem ser excluídas da lista de diretórios a serem percorridos
         dirs[:] = [d for d in dirs if d not in pastas_excluidas_set]
+        
         for file in files:
-            if file == "Dockerfile" or file.startswith("Dockerfile.") or any(file.endswith(ext) for ext in extensoes):
+            # Verifica se o arquivo é um dos arquivos específicos ou tem uma extensão desejada
+            if file in arquivos_especificos or any(file.endswith(ext) for ext in extensoes):
                 arquivos_encontrados.append(os.path.join(root, file))
     return arquivos_encontrados
 
@@ -60,10 +64,8 @@ def ler_conteudo_arquivo(caminho_arquivo):
         return f"Erro ao ler {caminho_arquivo}: {e}"
 
 def gerar_documentacao(saida_tree, arquivos_documentados, caminho_diretorio, somente_tree=False):
-    # Usa o nome da pasta e o timestamp de data e hora para o nome do arquivo
     nome_diretorio = os.path.basename(os.path.normpath(caminho_diretorio))
     timestamp = int(time.time())
-    #timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     nome_txt = f"{nome_diretorio}_{timestamp}.txt"
 
     with open(nome_txt, "w", encoding="utf-8") as file:
@@ -80,7 +82,7 @@ def gerar_documentacao(saida_tree, arquivos_documentados, caminho_diretorio, som
     print(f"Documento gerado: {nome_txt}")
 
 # Carrega as configurações do arquivo config.yml
-pastas_excluidas, extensoes_desejadas = carregar_configuracoes()
+pastas_excluidas, extensoes_desejadas, arquivos_especificos = carregar_configuracoes()
 
 # Usa o diretório atual se nenhum caminho for passado como argumento
 caminho_diretorio = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
@@ -88,13 +90,13 @@ caminho_diretorio = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
 # Executa o comando tree no caminho especificado
 saida_tree = executar_comando_tree(caminho_diretorio, pastas_excluidas)
 
-# Se extensoes_desejadas estiver vazio, o script gera o documento apenas com a estrutura de diretórios
-somente_tree = not extensoes_desejadas
+# Se extensoes_desejadas e arquivos_especificos estiverem vazios, o script gera o documento apenas com a estrutura de diretórios
+somente_tree = not (extensoes_desejadas or arquivos_especificos)
 
-# Encontra arquivos e lê o conteúdo (somente se houver extensões desejadas)
+# Encontra arquivos e lê o conteúdo (somente se houver extensões desejadas ou arquivos específicos)
 arquivos_documentados = {}
 if not somente_tree:
-    arquivos_encontrados = encontrar_arquivos(caminho_diretorio, extensoes_desejadas, pastas_excluidas)
+    arquivos_encontrados = encontrar_arquivos(caminho_diretorio, extensoes_desejadas, arquivos_especificos, pastas_excluidas)
     arquivos_documentados = {arquivo: ler_conteudo_arquivo(arquivo) for arquivo in arquivos_encontrados}
 
 # Gera o arquivo de documentação com ou sem conteúdo de arquivos, conforme configurado
